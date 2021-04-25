534f0d2abd3bb58fd0ba587a2e6684d9bb005ef9
Ndraaa· <78998317+Ependelope53@users.noreply.github.com>
2021-04-25 15:51:56 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -237,7 +237,7 @@ async def play(_, message: Message):
         await message.reply_photo(
         photo="final.png",
         reply_markup=keyboard,
-        caption=f"🏷 **Title:** [{title[:35]}]({url})\n **Duration:** {duration}\n" \
+        caption=f"🏷 **Title:** [{title[:35]}]({url})\n **⚡ Duration:** {duration}\n" \
                 + f"💡 **Status:** Playing\n🎧 **Requested by:** {requested_by}".format(
         message.from_user.mention()
         ),
