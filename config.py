e20b4c764670f903dd4dc4808514a7cdae29d146
zekiloi <80943695+zekiloi@users.noreply.github.com>
2021-11-03 16:41:28 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,6 @@ load_dotenv()
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
